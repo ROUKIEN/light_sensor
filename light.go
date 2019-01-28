e211301c7e4588dac3bf7bb2149078084ed018b2
Rukien <julien.jean@froupit.org>
2019-01-28 22:01:26 +0100

    [WIP] MQTT publishing

--- light.go
+++ light.go
@@ -2,16 +2,38 @@ package main
 
 import (
 	"bufio"
+	"encoding/json"
+	"fmt"
+	"github.com/eclipse/paho.mqtt.golang"
 	"github.com/tarm/serial"
 	"log"
+	"os"
 	"strconv"
+	"time"
 )
 
 type Payload struct {
 	Light int `json:"light"`
 }
 
+var f mqtt.MessageHandler = func(client mqtt.Client, msg mqtt.Message) {
+	fmt.Printf("TOPIC: %s\n", msg.Topic())
+	fmt.Printf("MSG: %s\n", msg.Payload())
+}
+
 func main() {
+	mqtt.DEBUG = log.New(os.Stdout, "", 0)
+	mqtt.ERROR = log.New(os.Stderr, "", 0)
+	opts := mqtt.NewClientOptions().AddBroker("localhost:1883").SetClientID("gotrivial")
+	opts.SetKeepAlive(2 * time.Second)
+	opts.SetDefaultPublishHandler(f)
+	opts.SetPingTimeout(1 * time.Second)
+
+	client := mqtt.NewClient(opts)
+	if token := client.Connect(); token.Wait() && token.Error() != nil {
+		panic(token.Error())
+	}
+
 	c := &serial.Config{Name: "/dev/ttyACM0", Baud: 9600}
 	s, err := serial.OpenPort(c)
 
@@ -22,12 +44,15 @@ func main() {
 	scanner := bufio.NewScanner(s)
 	for scanner.Scan() {
 		data := new(Payload)
-		i, err := strconv.Atoi(scanner.Text())
+		light, err := strconv.Atoi(scanner.Text())
 		if err != nil {
-			log.Printf("%v", err)
+			log.Printf("Error: %v", err)
 			continue
 		}
-		data.Light = i
-		log.Printf("%v\n", data.Light)
+
+		data.Light = light
+		jsonStr, _ := json.Marshal(data)
+		token := client.Publish("test_channel", 0, false, string(jsonStr))
+		token.Wait()
 	}
 }
